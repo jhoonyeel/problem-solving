6df5e90c4e1a5b8581c23f0999b4676828d3b9ad
dekoms <108325028+dekoms@users.noreply.github.com>
2024-01-25 04:06:00 +0900

    [Bronze II] Title: 소수 찾기, Time: 220 ms, Memory: 17804 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            int num = sc.nextInt();
+            if (num == 1) continue;
+
+            int div;
+            for (div = 2; div <= 1000 && div < num; div++) {
+                if (num % div == 0) {
+                    div = 0;
+                    break;
+                }
+            }
+            if (div != 0) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+
+        sc.close();
+
+    }
+}
