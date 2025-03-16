8310b15b4d47b7c8ff35b7522811e08fbe805b21
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-03-16 20:40:41 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -1,16 +1,12 @@
 def solution(emergency):
     N = len(emergency)
     res = [0] * N
-    last_big = 0
-    for order in range(N):
+    last_big = 10 ** 9
+    for order in range(1, N + 1):
         biggest = 0
         for el in emergency:
-            if not last_big:
-                if biggest < el:
-                    biggest = el
-            else:
-                if biggest < el and el < last_big:
-                    biggest = el
-        res[emergency.index(biggest)] = order + 1
+            if biggest < el < last_big:
+                biggest = el
+        res[emergency.index(biggest)] = order
         last_big = biggest
     return res
