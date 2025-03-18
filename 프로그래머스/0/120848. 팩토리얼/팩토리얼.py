cbc8714f0d843843eb7adfa0c5a6d7e88a19ef17
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-03-18 18:14:12 +0900

    [level 0] Title: 팩토리얼, Time: 0.00 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -1,7 +1,9 @@
 def solution(n):
+    # 초기 상태 1!으로 설정
     pac = 1
-    big_int = 2
-    while n >= pac:
-        pac *= big_int
-        big_int += 1
-    return big_int - 2
+    biggest = 1
+    # 이후 반복문에서 2!부터 시작
+    while pac <= n:
+        biggest += 1
+        pac *= biggest
+    return biggest - 1
