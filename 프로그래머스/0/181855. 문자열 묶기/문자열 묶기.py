57f15e44c6f7f2d889e9a464a565f2dd1dd5205c
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-03-20 00:54:07 +0900

    [level 0] Title: 문자열 묶기, Time: 15.22 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -1,9 +1,7 @@
 def solution(strArr):
-    order = [len(x) for x in strArr]
     size = {}
-    for length in order:
-        if length in size:
-            size[length] += 1
-        else:
-            size[length] = 1
+    for x in strArr:
+        size[len(x)] = size.get(len(x), 0) + 1
     return max(size.values())
+    # order = [len(x) for x in strArr]
+    # 리스트를 통해 len(x) 중복을 줄이면, 오히려 공간 복잡도가 더 커짐.
