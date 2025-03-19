e72a24a544117cc3c681a7df1fa2bbe2e4dcc85f
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-03-19 23:51:52 +0900

    [level 0] Title: 가까운 수, Time: 0.02 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -1,6 +1,2 @@
 def solution(array, n):
-    N = len(array)
-    minus_lst = []
-    for x in sorted(array):
-        minus_lst.append(abs(n - x))
-    return sorted(array)[minus_lst.index(min(minus_lst))]
+    return min([(abs(x - n), x) for x in array])[1]
