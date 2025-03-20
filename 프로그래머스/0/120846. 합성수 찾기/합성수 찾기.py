8d76032d84ed1b935f5786f1a278da06a02341ce
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-03-20 18:47:47 +0900

    [level 0] Title: 합성수 찾기, Time: 0.08 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,6 +1,6 @@
 def solution(n):
     cnt = 0
-    for x in range(1, n + 1):
+    for x in range(4, n + 1):
         for div in range(2, int(x ** 0.5) + 1):
             if x % div == 0:
                 cnt += 1
