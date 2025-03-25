6549e20453283abd95e6f83c6ac5cc824c011bd3
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-03-25 18:33:31 +0900

    [level 0] Title: 2의 영역, Time: 10.55 ms, Memory: 15.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -0,0 +1,15 @@
+def solution(arr):
+    l_idx, r_idx = -1, -1
+    for i, v in enumerate(arr):
+        if l_idx == -1 and v == 2:
+            l_idx, r_idx = i, i
+        if l_idx != -1 and v == 2:
+            r_idx = i
+    
+    if l_idx == -1:
+        return [-1]
+    elif l_idx == r_idx:
+        return arr[l_idx:r_idx + 1]
+    else:
+        return arr[l_idx:r_idx + 1]
+        
