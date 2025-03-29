e8899dfe02b83970d3f7e333afde20711b5907e5
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-03-30 00:40:11 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 187.01 ms, Memory: 30.5 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.py
@@ -2,7 +2,7 @@ def tuned(current):
     next_lst = []
     for el in current:
         if el >= 50 and el % 2 == 0:
-            next_lst.append(el / 2)
+            next_lst.append(el // 2)
         elif el < 50 and el % 2 != 0:
             next_lst.append(2 * el + 1)
     return next_lst
@@ -11,7 +11,9 @@ def solution(arr):
     cnt = 0
     lst, next_lst = arr[:], tuned(arr) # 0번 반복
     N = len(arr)
-    while set(lst) != set(next_lst):
+    while lst != next_lst:
         lst, next_lst = next_lst, tuned(next_lst)
         cnt += 1
     return cnt - 1
+    # lst == next_lst 는 주소 비교 X. 리스트 요소와 순서 판별.
+    # set(lst) != set(next_lst)는 순서 판별 X.
