4c81f0dae8cd17f164af7ee2f5895bbbb197f668
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-04-02 18:45:11 +0900

    [level 0] Title: 배열 만들기 6, Time: 202.12 ms, Memory: 17.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -0,0 +1,16 @@
+def solution(arr):
+    i = 0
+    stk = []
+    N = len(arr)
+    while i < N:
+        if not stk:
+            stk.append(arr[i])
+            i += 1
+            continue
+        if stk[len(stk) - 1] == arr[i]:
+            stk.pop()
+            i += 1
+        else:
+            stk.append(arr[i])
+            i += 1
+    return stk or [-1]
