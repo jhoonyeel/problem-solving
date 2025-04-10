0d0d73d2f5d393be936bdd7a2a714f65f4156fc7
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-04-11 00:23:27 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.05 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -1,11 +1,2 @@
 def solution(my_string):
-    res = [0] * 52
-    for x in my_string:
-        asci = ord(x)
-        if x.isupper():
-            res[asci - 65] += 1
-        else:
-            res[asci - 71] += 1
-    return res
-    # print(ord('A'), ord('a'))
-    # 65, 97
+    return [my_string.count(char) for char in 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz']
