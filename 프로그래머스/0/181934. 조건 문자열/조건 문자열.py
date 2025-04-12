b1a70a0e4515c9ffeec4d20234ae727f393fa68e
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-04-12 18:43:35 +0900

    [level 0] Title: 조건 문자열, Time: 0.01 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -1,11 +1,11 @@
 def solution(ineq, eq, n, m):
-    if eq == '=':
-        if ineq == '>':
-            return int(n >= m)            
-        else:
-            return int(n <= m)
-    elif eq == '!':
-        if ineq == '>':
-            return int(n > m)            
-        else:
-            return int(n < m)
+    op = ineq + eq
+    d = {
+        '>=': lambda a, b: a >= b,
+        '<=': lambda a, b: a <= b,
+        '>!': lambda a, b: a > b,
+        '<!': lambda a, b: a < b
+    }
+    return int(d[op](n, m))
+        # 많은 조건을 처리해야 할 때 (if-elif가 길어질 때),
+        # lambda + dict 고려.
