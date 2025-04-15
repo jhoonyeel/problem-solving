56abeb9cedcf64ed0a1ce18f8b2926860fc38135
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-04-15 15:09:23 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.04 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -1,5 +1,7 @@
 def solution(my_string, overwrite_string, s):
-    return my_string[:s] + overwrite_string + my_string[s + len(overwrite_string):]
-    # my_string = "bbawwbb"
-    # overwrite_string = "XX"
-    # XXawwXX. replace는 모두 바뀜.
+    lst1 = list(my_string)
+    lst2 = list(overwrite_string)
+    lst1[s:s + len(lst2)] = lst2
+    return ''.join(lst1)
+    # 리스트의 슬라이스 구간에 b 리스트의 "요소"를 할당
+    # 문자열은 immutable(불변) → 부분 수정 불가
