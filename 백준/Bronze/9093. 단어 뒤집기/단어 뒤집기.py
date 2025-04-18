e6cff6b88b29f5f9611f38ef322ae1837c252eba
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-04-18 18:06:55 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 272 ms, Memory: 40600 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -1,7 +1,6 @@
 n = int(input())
-lines = [input() for _ in range(n)]
+lines = [input().split() for _ in range(n)]
 
 for line in lines:
-  lst = line.split()
-  reverse = [list(el)[::-1] for el in lst]
-  print(' '.join([''.join(l) for l in reverse]))
+  reverse = [word[::-1] for word in line]
+  print(' '.join(reverse))
