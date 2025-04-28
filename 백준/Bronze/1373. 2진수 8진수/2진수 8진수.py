cdb93d6c9bcee4fc2ffdfc5350538e13fc5ce45f
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-04-29 01:40:03 +0900

    [Bronze I] Title: 2진수 8진수, Time: 44 ms, Memory: 34368 KB -BaekjoonHub

--- 백준/Bronze/1373. 2진수 8진수/2진수 8진수.py
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.py
@@ -1,11 +1,4 @@
-binary = input()
+binary = input() 
 
-n = len(binary)
-if n % 3 != 0:
-  binary = '0' * (3 - (n % 3))+ binary
-
-res = ''
-for x in range(0, len(binary), 3):
-  three_size = binary[x:x + 3]
-  res += str(int(three_size, 2))
-print(res)
+decimal = int(binary, 2) 
+print(oct(decimal)[2:])
