ef61bf518443a828dc1f8febc791e1485b35a7fe
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-05-04 00:20:32 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 184 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
@@ -0,0 +1,10 @@
+N = int(input())
+price = list(map(int, input().split()))
+
+dp = [float('inf')] * (N + 1)
+dp[0] = 0
+
+for i in range(1, N + 1):
+  for j in range(1, i + 1):
+    dp[i] = min(dp[i], dp[i - j] + price[j - 1])
+print(dp[N])
