0ac784746bff4b0bfb8d633b56ab28be500dda6b
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-06-29 03:44:05 +0900

    [Silver II] Title: 친구, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1058. 친구/친구.py
+++ 백준/Silver/1058. 친구/친구.py
@@ -1,19 +1,20 @@
 N = int(input())
-
 board = [list(input()) for _ in range(N)]
 
-max_cnt = 0
+cnt = [0] * N
+
 for i in range(N):
-  friends = set()
-  for j in range(N):
-    if i == j:
-      continue
-    if board[i][j] == 'Y':
-      friends.add(j)
-    else:
-      for k in range(N):
-        if board[i][k] == 'Y' and board[k][j] == 'Y':
-          friends.add(j)
-          break
-  max_cnt = max(max_cnt, len(friends))
-print(max_cnt)
+    for j in range(i + 1, N):
+        # i와 j가 친구이거나, 공통 친구가 존재하는 경우
+        if board[i][j] == 'Y':
+            cnt[i] += 1
+            cnt[j] += 1
+        else:
+            for k in range(N):
+                if k != i and k != j:
+                    if board[i][k] == 'Y' and board[k][j] == 'Y':
+                        cnt[i] += 1
+                        cnt[j] += 1
+                        break  # 공통 친구 한 명만 확인되면 충분
+
+print(max(cnt))
