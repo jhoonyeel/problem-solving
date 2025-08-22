6ec43eae075c5a5d6d3afea46f76563b83b7cab8
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-08-22 17:05:24 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 64 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -1,19 +1,14 @@
 T = int(input())
+MAX = 10000
 
-MAX = 10001
-dp = [[0] * 4 for _ in range(MAX)]  # dp[n][k]: n을 1~k를 써서 만드는 조합 수
+dp = [0] * (MAX + 1)
+dp[0] = 1   # 0을 만드는 방법은 1가지 (아무것도 안 쓰는 것)
 
-# 초기 조건
-for i in range(1, 4):  # 숫자 1, 2, 3
-  dp[0][i] = 1  # 아무것도 안 쓰는 경우도 1개로 셈
-
-for n in range(1, MAX):
-  for k in range(1, 4):  # 숫자 1, 2, 3
-    if n - k >= 0:
-      dp[n][k] = dp[n][k - 1] + dp[n - k][k]
-    else:
-      dp[n][k] = dp[n][k - 1]  # 더 이상 해당 숫자 못 쓰는 경우
+# 동전(=1,2,3)을 차례대로 적용
+for coin in [1, 2, 3]:
+  for j in range(coin, MAX + 1):
+    dp[j] += dp[j - coin]
 
 for _ in range(T):
   n = int(input())
-  print(dp[n][3])  # 1,2,3까지 고려한 조합 수
+  print(dp[n])
