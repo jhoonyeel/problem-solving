43d7d54f7f88e728944f5a7038bc1a56d705fecd
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-08-31 23:59:25 +0900

    [Silver I] Title: 에너지 모으기, Time: 68 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/16198. 에너지 모으기/에너지 모으기.py
+++ 백준/Silver/16198. 에너지 모으기/에너지 모으기.py
@@ -1,19 +1,17 @@
 N = int(input())
-
-weights = list(map(int, input().split()))
+Wi = list(map(int, input().split()))
 
 max_energy = 0
-def dfs(arr, total):
+def dfs(arr, energy):
   global max_energy
-
   if len(arr) == 2:
-    max_energy = max(max_energy, total)
+    max_energy = max(max_energy, energy)
     return
-
+  
   for i in range(1, len(arr) - 1):
-    gain = arr[i - 1] * arr[i + 1]
+    val = arr[i-1] * arr[i+1]
     removed = arr.pop(i)
-    dfs(arr, total + gain)
-    arr.insert(i, removed)  # 백트래킹
-dfs(weights, 0)
+    dfs(arr, energy + val)
+    arr.insert(i, removed)
+dfs(Wi[:], 0)
 print(max_energy)
