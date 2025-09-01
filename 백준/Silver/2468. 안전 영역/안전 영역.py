86a919dbc3516823bcf8a11d1fe319e803a32337
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-09-01 23:33:38 +0900

    [Silver I] Title: 안전 영역, Time: 628 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,41 +1,38 @@
 from collections import deque
 N = int(input())
-
 board = [list(map(int, input().split())) for _ in range(N)]
 
-MAX = 0
-for i in range(N):
-  MAX = max(max(board[i]), MAX)
+def max2inArr(arr2):
+  max_val = 0
+  for i in range(N):
+    for j in range(N):
+      max_val = max(max_val, arr2[i][j])
+  return max_val
 
-can_go = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 
-def bfs(x, y, h, visited):
+def bfs(x, y, visited, h):
   q = deque()
-  q.append((x, y))
   visited[x][y] = True
+  q.append((x, y))
   
   while q:
     x, y = q.popleft()
-    visited[x][y] = True
-    
-    for dx, dy in can_go:
+    for dx, dy in dirs:
       nx, ny = x + dx, y + dy
-      if 0 <= nx <= N-1 and 0 <= ny <= N-1:
-        if not visited[nx][ny] and board[nx][ny] > h:
+      if (0 <= nx < N and 0 <= ny < N) and not visited[nx][ny]:
+        if board[nx][ny] > h:
           visited[nx][ny] = True
           q.append((nx, ny))
+  return 1
 
-max_safe_area_cnt = 0
-for h in range(0, MAX): # 비가 0부터 MAX-1까지 옴.
+max_val = 0
+for h in range(max2inArr(board) + 1):
   visited = [[False] * N for _ in range(N)]
-  safe_area_cnt = 0
-
+  cnt = 0
   for i in range(N):
     for j in range(N):
       if not visited[i][j] and board[i][j] > h:
-        bfs(i, j, h, visited)
-        safe_area_cnt += 1
-
-  max_safe_area_cnt = max(max_safe_area_cnt, safe_area_cnt)
-
-print(max_safe_area_cnt)
+        cnt += bfs(i, j, visited, h)
+  max_val = max(max_val, cnt)
+print(max_val)
