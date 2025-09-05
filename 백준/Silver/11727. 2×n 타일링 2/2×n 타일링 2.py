aa79553ed6255f245ee0062aa37918174bcd4b0e
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-09-05 17:31:12 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -1,10 +1,11 @@
-n = int(input())
-dp = [0] * (n + 1)
-
+N = int(input())
+MOD = 10_007
+dp = [0] * (N + 1)
+# dp[i] = 2 * i 방법의 수
 dp[1] = 1
-if n >= 2:
+if N >= 2:
   dp[2] = 3
 
-for i in range(3, n + 1):
-  dp[i] = dp[i - 1] + (dp[i - 2] * 2)
-print(dp[n] % 10_007)
+for i in range(3, N + 1):
+  dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % MOD
+print(dp[N])
