91edf4ccb8375411ce31d7bb3feea9ae6af1cee6
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-09-10 00:02:48 +0900

    [Silver I] Title: 스티커, Time: 984 ms, Memory: 63520 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -1,15 +1,16 @@
 T = int(input())
 for _ in range(T):
   n = int(input())
-  top, bottom = list(map(int, input().split())), list(map(int, input().split()))
+  board = [list(map(int, input().split())) for _ in range(2)]
   
-  dp = [[0] * 3 for _ in range(n + 1)]
-  dp[1][0] = 0
-  dp[1][1] = top[0]
-  dp[1][2] = bottom[0]
+  dp = [[0] * 3 for _ in range(n)]
+  # dp[i][j]: i번째 열에 j행을 선택한 최댓값
   
-  for i in range(2, n + 1):
-    dp[i][0] = max(dp[i - 1][1], dp[i - 1][2])
-    dp[i][1] = max(dp[i - 1][0], dp[i - 1][2]) + top[i - 1]
-    dp[i][2] = max(dp[i - 1][0], dp[i - 1][1]) + bottom[i - 1]
-  print(max(dp[n]))
+  dp[0][0] = board[0][0]
+  dp[0][1] = board[1][0]
+  dp[0][2] = 0
+  for i in range(1, n):
+    dp[i][0] = max(dp[i - 1][1], dp[i - 1][2]) + board[0][i]
+    dp[i][1] = max(dp[i - 1][0], dp[i - 1][2]) + board[1][i]
+    dp[i][2] = max(dp[i - 1][0], dp[i - 1][1], dp[i - 1][2])
+  print(max(dp[n - 1][0], dp[n - 1][1], dp[n - 1][2]))
