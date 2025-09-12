89101007344a4db1da4df7110feeceb3b2c8bd87
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-09-13 01:55:43 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 140 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.py
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.py
@@ -1,11 +1,17 @@
 N = int(input())
 Ai = list(map(int, input().split()))
 
-dp = [0] * (N + 1)
+dp = [1] * (N + 1)
+# dp[i]: i번째 요소를 마지막으로 하는 부분 수열의 길이 최댓값
 
 for i in range(1, N + 1):
-  dp[i] = 1
-  for j in range(1, i + 1):
-    if Ai[j - 1] > Ai[i - 1]:
-      dp[i] = max(dp[i], dp[j] + 1)
+  for k in range(1, i):
+    if Ai[k - 1] > Ai[i - 1]:
+      dp[i] = max(dp[i], dp[k] + 1)
 print(max(dp))
+
+# 발생할 수 있는 모든 경우
+# 현재 바라보는 요소를 선택할지 말지 결정한다.
+# (현재 바라보는 요소 < 부분 수열의 마지막 요소)
+# 1. Yes
+# 2. No
