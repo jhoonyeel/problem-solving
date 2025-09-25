14573eb11d33284ed0fda6c3d64fd8b32cab2487
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-09-25 16:42:57 +0900

    [Silver IV] Title: 큐, Time: 64 ms, Memory: 34936 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -1,29 +1,21 @@
 from collections import deque
 import sys
 input = sys.stdin.readline
-queue = deque()
+N = int(input())
 
-d = {
-  'push': lambda x: queue.append(x),
-  'pop': lambda: queue.popleft() if queue else -1,
-  'size': lambda: len(queue),
-  'empty': lambda: 0 if queue else 1,
-  'front': lambda: queue[0] if queue else -1,
-  'back': lambda: queue[-1] if queue else -1,
+q = deque()
+cmd = {
+  'push': lambda x: q.append(x),
+  'pop': lambda: q.popleft() if q else -1,
+  'size': lambda: len(q),
+  'empty': lambda: 0 if q else 1,
+  'front': lambda: q[0] if q else -1,
+  'back': lambda: q[-1] if q else -1,
 }
 
-n = int(input().rstrip())
-for _ in range(n):
-  command = input().rstrip()
-  if command.split()[0] == 'push':
-    d['push'](command.split()[1])
-  elif command == 'pop':
-    print(d['pop']())
-  elif command == 'size':
-    print(d['size']())
-  elif command == 'empty':
-    print(d['empty']())
-  elif command == 'front':
-    print(d['front']())
-  elif command == 'back':
-    print(d['back']())
+for i in range(N):
+  line = input().split()
+  if line[0] == 'push':
+    cmd['push'](line[1])
+  else:
+    print(cmd[line[0]]())
