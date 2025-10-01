28e4d577f09f5f6996b5791861c1f38106e9100f
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-10-01 23:22:44 +0900

    [Silver IV] Title: GCD 합, Time: 184 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/9613. GCD 합/GCD 합.py
+++ 백준/Silver/9613. GCD 합/GCD 합.py
@@ -1,18 +1,12 @@
-import sys
-input = sys.stdin.readline
+import math
+T = int(input())
 
-def uqlid(x, y):
-  while y > 0:
-    x, y = y, x % y
-  return x
-
-t = int(input())
-
-for _ in range(t):
+for _ in range(T):
   line = list(map(int, input().split()))
-  n, vals = line[0], sorted(line[1:])
-  gcd_sum = 0
-  for i in range(n - 1):
+  n, l = line[0], sorted(line[1:])
+
+  res = 0
+  for i in range(n):
     for j in range(i + 1, n):
-      gcd_sum += uqlid(vals[i], vals[j])
-  print(gcd_sum)
+      res += math.gcd(l[i], l[j])
+  print(res)
