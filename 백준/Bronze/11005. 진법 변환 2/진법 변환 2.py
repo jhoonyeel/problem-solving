87d31a314d3fa49ef23bbdd6bd133774a1c07a75
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-10-02 00:23:55 +0900

    [Bronze I] Title: 진법 변환 2, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -0,0 +1,18 @@
+N, B = map(int, input().split())
+
+if N == 0:
+  print(0)
+  exit()
+
+p = 1
+while B ** p <= N:
+  p += 1
+
+res = []
+for i in range(p - 1, -1, -1):  # (p-1)자리부터 0자리까지
+  cnt = N // (B ** i)       # 현재 자리의 값
+  N %= B ** i               # 남은 값 갱신
+  res.append(cnt)
+
+r = [chr(55 + x) if x >= 10 else x for x in res]
+print(*r, sep='')
