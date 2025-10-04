836d84f6dabb7b47878c3c814ae2f9642b1cdb80
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-10-05 04:33:57 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 2700 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,27 +1,27 @@
 import sys
-
 MAX = 1_000_000
+
+# 에라토스테네스의 체
 is_prime = [True] * (MAX + 1)
-is_prime[0] = False
-is_prime[1] = False
+is_prime[0] = is_prime[1] = False
 
 for i in range(2, int(MAX ** 0.5) + 1):
   if is_prime[i]:
     for j in range(i * i, MAX + 1, i):
       is_prime[j] = False
 
-inputs = list(map(int, sys.stdin.read().split()))
-for n in inputs:
-  if n == 0:
+input = sys.stdin.readline
+while True:
+  N = int(input())
+  if N == 0:
     break
   
-  is_possible = False
-  for a in range(3, n // 2 + 1, 2):
-    if is_prime[a] and is_prime[n - a]:
-      is_possible = True
+  found = False
+  for a in range(3, N, 2):
+    if is_prime[a] and is_prime[N - a]:
+      print(f"{N} = {a} + {N - a}")
+      found = True
       break
   
-  if is_possible:
-    print(f"{n} = {a} + {n - a}")
-  else:
+  if not found:
     print("Goldbach's conjecture is wrong.")
