8b4b11d712cd979a2c8eff6e127eefbe71e48704
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-10-07 00:06:42 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -1,15 +1,7 @@
 N = int(input())
 
-res = 1
-for x in range(2, N + 1):
-  res *= x
-
-cnt = 0
-while True:
-  if res % 10 == 0:
-    cnt += 1
-    res //= 10
-    continue
-  else:
-    break
-print(cnt)
+count = 0
+while N >= 5:
+  count += N // 5
+  N //= 5
+print(count)
