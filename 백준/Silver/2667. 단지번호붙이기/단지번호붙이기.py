3e46e5d7c00378191b9cdb8255052036353168f3
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-10-15 23:56:34 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,36 +1,32 @@
 from collections import deque
 N = int(input())
-board = [list(map(int, list(input()))) for _ in range(N)]
+board = [list(map(int, input())) for _ in range(N)]
 
-dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+direction = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
-visited = [[False] * N for _ in range(N)]
-def bfs(x, y):
-  q = deque()
+def bfs(visited, x, y):
+  q = deque([(x, y)])
   visited[x][y] = True
-  q.append((x, y))
-  size = 1
-  
+  cnt = 1
   while q:
     x, y = q.popleft()
-    
-    for dx, dy in dirs:
-      nx, ny = x + dx, y + dy
+    for dx, dy in direction:
+      nx, ny = dx + x, dy + y
       if 0 <= nx < N and 0 <= ny < N:
-        if board[nx][ny] == 0:
-          continue
-        if not visited[nx][ny]:
-          visited[nx][ny] = True
+        if not visited[nx][ny] and board[nx][ny] == 1:
+          cnt += 1
           q.append((nx, ny))
-          size += 1
-  return size
+          visited[nx][ny] = True
+  return cnt
 
+visited = [[False] * N for _ in range(N)]
 res = []
 for i in range(N):
   for j in range(N):
     if board[i][j] == 1 and not visited[i][j]:
-      cnt = bfs(i, j)
+      cnt = bfs(visited, i, j)
       res.append(cnt)
+
 print(len(res))
 for x in sorted(res):
   print(x)
