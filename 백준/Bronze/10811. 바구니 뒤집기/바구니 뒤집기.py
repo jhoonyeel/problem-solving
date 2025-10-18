3ec3a8a26db308263ba8743de53d8998c8284052
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-10-19 00:00:58 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -3,5 +3,5 @@ N, M = map(int, input().split())
 seq = list(range(1, N + 1))
 for _ in range(M):
   start, end = list(map(int, input().split()))
-  seq = seq[:start - 1] + list(reversed(seq[start - 1:end])) + seq[end:]
+  seq[start - 1:end] = seq[start - 1:end][::-1]
 print(*seq)
