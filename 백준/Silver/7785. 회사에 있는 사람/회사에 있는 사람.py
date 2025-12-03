0e2bffd7d371b2945973130bba6253286779ad76
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-12-03 16:49:53 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 2508 ms, Memory: 48360 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -1,22 +1,14 @@
 n = int(input())
 
-is_in_company = {
-  "enter": True,
-  "leave": False,
-}
-
 logs = {}
 for _ in range(n):
   name, entry = input().split()
-  if name not in logs:
-    logs.setdefault(name, entry)
-  else:
-    logs[name] = entry
+  logs[name] = entry
 
 res = []
-for k, v in logs.items():
-  if is_in_company[v]:
-    res.append(k)
+for name, entry in logs.items():
+  if entry == "enter":
+    res.append(name)
 
-res.sort(key=lambda x: x, reverse=True)
-print('\n'.join(res))
+res.sort(reverse=True)
+print("\n".join(res))
