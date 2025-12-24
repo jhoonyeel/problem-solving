c8fa4edac61557bbe6336e0f9b57fc584dd4525b
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-12-24 12:08:38 +0900

    [Bronze V] Title: 나머지, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.py
+++ 백준/Bronze/10430. 나머지/나머지.py
@@ -1,6 +1,2 @@
 A, B, C = map(int, input().split())
-
-print((A + B) % C)
-print(((A % C) + (B % C)) % C)
-print((A * B) % C)
-print(((A % C) * (B % C)) % C)
+print((A + B) % C, ((A % C) + (B % C)) % C, (A * B) % C, ((A % C) * (B % C)) % C)
