e57827054e72ef50d605a639d21cfd3fc2f5695a
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2025-12-31 00:17:10 +0900

    [Bronze I] Title: 소인수분해, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,11 +1,14 @@
 N = int(input())
 
-MAX = int(N ** 0.5) + 1
+MAX = int(N ** 0.5)
+res = []
 
 for x in range(2, MAX + 1):
   while N % x == 0:
-    print(x)
     N //= x
+    res.append(x)
 
 if N > 1:
-  print(N)
+  res.append(N)
+
+print('\n'.join(map(str, res)))
