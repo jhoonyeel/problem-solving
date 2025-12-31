6b9f2e0381b40f1747fc190d25c973a05b83fc88
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-01-01 00:00:44 +0900

    [Bronze IV] Title: 모음의 개수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1264. 모음의 개수/모음의 개수.py
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.py
@@ -1,15 +1,10 @@
-MOEUM = 'aeiou'
-
 while True:
-  line = input().split()
-  if line[0] == '#':
+  line = input()
+  if line == '#':
     break
   
   cnt = 0
-  for x in line:
-    if not x.islower():
-      x = x.lower()
-    for ch in x:
-      if ch in MOEUM:
-        cnt += 1
+  for ch in line:
+    if ch in 'aeiouAEIOU':
+      cnt += 1
   print(cnt)
