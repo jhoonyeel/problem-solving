576624b1c2bd5022bc6a76eeada07c1848890f31
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-01-07 18:03:30 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -1,14 +1,13 @@
 A, B, V = map(int, input().split())
 
-# 이미 첫 날에 도착 가능한 경우
-if V <= A:
-  print(1)
+day_sun = 1
+height = A
+
+diff = A - B
+cnt = (V - height) // diff
+remain = (V - height) % diff
+
+if remain == 0:
+  print(day_sun + cnt)
 else:
-  V -= A          # 마지막 날에 올라갈 높이를 미리 빼줌
-  daily = A - B   # 하루 순 상승량
-
-  days = V // daily
-  if V % daily != 0:
-    days += 1
-
-  print(days + 1)  # 마지막 날
+  print(day_sun + cnt + 1)
