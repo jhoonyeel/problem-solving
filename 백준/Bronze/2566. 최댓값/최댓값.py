1e94317a941b084b35b97ceda45aa6cf4663a7ea
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-01-14 23:32:04 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -1,12 +1,14 @@
-matrix = [list(input().split()) for _ in range(9)]
+board = [list(map(int, input().split())) for _ in range(9)]
 
-biggest = float('-inf')
-row, col = 0, 0
-for i in range(9):
-  for j in range(9):
-    int_val = int(matrix[i][j])
-    if int_val > biggest:
-      biggest = int_val
-      row, col = i, j
-print(biggest)
-print(row + 1, col + 1)
+max_value = float('-inf')
+r = 0
+for idx, row in enumerate(board):
+  m = max(row)
+  if max_value < m:
+    max_value = m
+    r = idx
+
+c = board[r].index(max_value)
+
+print(max_value)
+print(r + 1, c + 1)
