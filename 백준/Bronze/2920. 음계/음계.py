83df657b0a895cd1e6ede1602239d1c3fba1412a
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-01-17 22:37:59 +0900

    [Bronze II] Title: 음계, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,10 +1,10 @@
-asc = list(range(1, 9))
-desc = list(range(8, 0, - 1))
-line = list(map(int, input().split()))
+ASC = list(range(1, 9))
+DESC = list(range(8, 0, -1))
 
-if line == asc:
+line = list(map(int, input().split()))
+if line == ASC:
   print('ascending')
-elif line == desc:
+elif line == DESC:
   print('descending')
 else:
   print('mixed')
