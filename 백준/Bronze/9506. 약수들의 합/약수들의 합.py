0cdd87ee6478304eb9534628c464e2b41c6f8fd4
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-01-19 00:46:23 +0900

    [Bronze I] Title: 약수들의 합, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.py
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -1,15 +1,19 @@
 import sys
 
+def get_divisors(n):
+  res = []
+  for i in range(1, n // 2 + 1):
+    if n % i == 0:
+      res.append(i)
+
+  return res
+
 for line in sys.stdin:
   n = int(line.rstrip())
   if n == -1:
     break
   
-  res = []
-  for i in range(1, n // 2 + 1):
-    if n % i == 0:
-      res.append(i)
-  
+  res = get_divisors(n)
   total = sum(res)
   if total == n:
     print(f"{n} = {' + '.join(map(str, res))}")
