974aa449eb32e183b52beee992a88681307900bd
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-01-20 05:04:00 +0900

    [Silver III] Title: 소수 구하기, Time: 1772 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,14 +1,18 @@
 M, N = map(int, input().split())
 
 def is_prime(x):
+  if x <= 1:
+    return False
   if x == 2:
     return True
-  if x == 1 or x % 2 == 0:
+  if x % 2 == 0:
     return False
-  for y in range(3, int(x ** 0.5) + 1, 2):
-    if x % y == 0:
+  
+  for i in range(3, int(x ** 0.5) + 1, 2):
+    if x % i == 0:
       return False
   return True
+
 for x in range(M, N + 1):
   if is_prime(x):
     print(x)
