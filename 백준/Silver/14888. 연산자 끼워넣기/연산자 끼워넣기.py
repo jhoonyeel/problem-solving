0bd6153c9e995e450ab269ec87ba77280e6b670f
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-01-25 23:19:34 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 64 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,29 +1,36 @@
+import sys
+sys.setrecursionlimit(10**6)
+
 N = int(input())
 Ai = list(map(int, input().split()))
 op_cnt = list(map(int, input().split()))
 
-cal = {
+d = {
   0: lambda x, y: x + y,
   1: lambda x, y: x - y,
   2: lambda x, y: x * y,
-  3: lambda x, y: x // y if x > 0 else -(abs(x) // y),
+  3: lambda x, y: -(-x // y) if x < 0 else x // y,
 }
 
-min_val, max_val = float('inf'), float('-inf')
-visited = op_cnt[:]
-def dfs(start, acc, visited):
-  global min_val, max_val
+max_val = float('-inf')
+min_val = float('inf')
+
+def dfs(idx, cur, op):
+  global max_val, min_val
   
-  if start == N - 1:
-    min_val = min(acc, min_val)
-    max_val = max(acc, max_val)
+  if idx == N - 1:
+    max_val = max(max_val, cur)
+    min_val = min(min_val, cur)
     return
   
-  for i in range(4):
-    if visited[i] > 0:
-      visited[i] -= 1
-      dfs(start + 1, cal[i](acc, Ai[start + 1]), visited)
-      visited[i] += 1
-dfs(0, Ai[0], visited)
+  for cmd in range(4):
+    if op[cmd] > 0:
+      nxt_cur = d[cmd](cur, Ai[idx + 1])
+      op[cmd] -= 1
+      dfs(idx + 1, nxt_cur, op)
+      op[cmd] += 1
+
+dfs(0, Ai[0], op_cnt[:])
+
 print(max_val)
 print(min_val)
