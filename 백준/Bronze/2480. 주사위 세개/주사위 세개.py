7757646644a65677b0a73bea74ec7c979bc60fbe
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-01-31 23:42:22 +0900

    [Bronze IV] Title: 주사위 세개, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,12 +1,14 @@
-a, b, c = map(int, input().split())
+numbers = list(map(int, input().split()))
 
-price = 0
-if a == b == c:
-  price = 10_000 + a * 1_000
-elif a == b or a == c:
-  price = 1000 + a * 100
-elif b == c:
-  price = 1000 + b * 100
+is_same_triple = numbers[0] == numbers[1] and numbers[1] == numbers[2]
+is_same_double = numbers[0] == numbers[1] or numbers[1] == numbers[2] or numbers[0] == numbers[2]
+
+res = 0
+if is_same_triple:
+  res = 10_000 + numbers[0] * 1_000
+elif is_same_double:
+  l = sorted(numbers)
+  res = 1_000 + l[1] * 100
 else:
-  price = max(a, b, c) * 100
-print(price)
+  res = max(numbers) * 100
+print(res)
