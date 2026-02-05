2f37d48a9a8bf71740983464f4cff256b33593c5
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-02-05 23:57:49 +0900

    [Silver V] Title: 행렬 곱셈, Time: 172 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.py
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.py
@@ -1,16 +1,17 @@
 N, M = map(int, input().split())
 A = [list(map(int, input().split())) for _ in range(N)]
-
-M, K = map(int, input().split())
+_, K = map(int, input().split())
 B = [list(map(int, input().split())) for _ in range(M)]
 
-# 결과 행렬 N x K
-C = [[0] * K for _ in range(N)]
-
+res = []
 for i in range(N):
+  line = []
   for j in range(K):
-    for t in range(M):
-      C[i][j] += A[i][t] * B[t][j]
+    s = 0
+    for k in range(M):
+      s += A[i][k] * B[k][j]
+    line.append(s)
+  res.append(line)
 
-for row in C:
-  print(*row)
+for l in res:
+  print(*l)
