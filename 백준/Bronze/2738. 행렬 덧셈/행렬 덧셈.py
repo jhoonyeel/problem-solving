0e9cb42e40ed7d8fed95b59a1a101f75efd5aa24
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-02-05 23:47:42 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,9 +1,13 @@
 N, M = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(N)]
+B = [list(map(int, input().split())) for _ in range(N)]
 
-N_list = [list(map(int, input().split())) for _ in range(N)]
-M_list = [list(map(int, input().split())) for _ in range(N)]
+res = []
+for a, b in zip(A, B):
+  line = []
+  for x, y in zip(a, b):
+    line.append(x + y)
+  res.append(line)
 
-for i in range(N):
-  for j in range(M):
-    print(N_list[i][j] + M_list[i][j], end=' ')
-  print()
+for l in res:
+  print(*l)
