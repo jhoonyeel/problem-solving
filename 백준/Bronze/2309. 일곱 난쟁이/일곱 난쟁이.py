ad2ba531e6f9fdca4045fa5eeaacbacf227d2a20
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-02-07 22:54:02 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,22 +1,13 @@
-SEVEN = 100
-height_list = [int(input()) for _ in range(9)]
+heights = [int(input()) for _ in range(9)]
+total = sum(heights)
 
-nine = sum(height_list)
-delete_sum = nine - SEVEN
-
-delete_x, delete_y = 0, 0
 for i in range(9):
   for j in range(i + 1, 9):
-    if height_list[i] + height_list[j] == delete_sum:
-      delete_x = height_list[i]
-      delete_y = height_list[j]
-      break
-  if delete_x != 0:
-    break
-
-height_list.remove(delete_x)
-height_list.remove(delete_y)
-
-height_list.sort()
-for x in height_list:
-  print(x)
+    if total - heights[i] - heights[j] == 100:
+      res = []
+      for k in range(9):
+        if k != i and k != j:
+          res.append(heights[k])
+      for x in sorted(res):
+        print(x)
+      exit()
