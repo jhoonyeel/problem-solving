5e8e7c9ec45a3571a915047b2be0099df106e77f
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-02-08 23:41:09 +0900

    [Silver III] Title: 등수 매기기, Time: 516 ms, Memory: 54820 KB -BaekjoonHub

--- 백준/Silver/2012. 등수 매기기/등수 매기기.py
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.py
@@ -2,12 +2,10 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
+predict_grade = sorted(int(input()) for _ in range(N))
 
-except_grade = [int(input()) for _ in range(N)]
-
-lst = sorted(except_grade)
-
-res = 0
-for i in range(N):
-  res += abs(lst[i] - (i + 1))
-print(res)
+total = 0
+for idx, v in enumerate(predict_grade):
+  angry = abs(v - (idx + 1))
+  total += angry
+print(total)
