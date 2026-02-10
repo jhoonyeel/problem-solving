decededb1eb7a82c0417d2d85c77040603d2e66a
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-02-10 23:06:34 +0900

    [Silver V] Title: Base Conversion, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11576. Base Conversion/Base Conversion.py
+++ 백준/Silver/11576. Base Conversion/Base Conversion.py
@@ -1,15 +1,17 @@
-A, B = map(int, input().split())
+toA, toB = map(int, input().split())
 m = int(input())
-Anums = list(map(int, input().split()))
+nums_from_A = list(map(int, input().split()))
 
-# 1) A진법 → 10진법
-decimal = 0
-for num in Anums:
-  decimal = decimal * A + num
+digit = 0
+for idx, v in enumerate(nums_from_A[::-1]):
+  digit += v * (toA ** idx)
 
-# 2) 10진법 → B진법
 res = []
-while decimal > 0:
-  decimal, r = divmod(decimal, B)
-  res.append(str(r))
-print(' '.join(reversed(res)))
+if digit == 0:
+  res.append(0)
+else:
+  while digit > 0:
+    res.append(digit % toB)
+    digit //= toB
+
+print(' '.join(map(str, res[::-1])))
