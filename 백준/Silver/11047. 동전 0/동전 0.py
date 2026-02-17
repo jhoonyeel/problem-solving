944b09f41fcaa8620960f265654a4b6cf2c28ee5
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-02-17 23:45:25 +0900

    [Silver IV] Title: 동전 0, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -2,7 +2,7 @@ N, K = map(int, input().split())
 coins = [int(input()) for _ in range(N)]
 
 cnt = 0
-for coin in reversed(coins):  # 큰 동전부터 탐색
+for coin in coins[::-1]:
   if coin <= K:
     cnt += K // coin
     K %= coin
