b28b7ef361e49685ab61dbdccb7d8e38743c0c5d
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-02-19 23:57:44 +0900

    [Bronze II] Title: 분산처리, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.py
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -1,25 +1,20 @@
 T = int(input())
-
-cycles = {
-  1: [1],
-  2: [2, 4, 8, 6],
-  3: [3, 9, 7, 1],
-  4: [4, 6],
-  5: [5],
-  6: [6],
-  7: [7, 9, 3, 1],
-  8: [8, 4, 2, 6],
-  9: [9, 1]
-}
-
 for _ in range(T):
   a, b = map(int, input().split())
-  a %= 10
-
-  if a == 0:
+  
+  cur = 1
+  subfix = []
+  while True:
+    cur *= a
+    sub = cur % 10
+    if sub in subfix:
+      break
+    subfix.append(sub)
+  
+  idx = (b - 1) % len(subfix)
+  res = subfix[idx]
+  
+  if res == 0:
     print(10)
-    continue
-
-  cycle = cycles[a]
-  index = (b - 1) % len(cycle)
-  print(cycle[index])
+  else:
+    print(res)
