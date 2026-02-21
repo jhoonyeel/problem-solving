41a8ba1e7304b81899288d8aad15f02c47c79482
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-02-22 00:00:07 +0900

    [Gold V] Title: 회의실 배정, Time: 2344 ms, Memory: 61144 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,15 +1,12 @@
 N = int(input())
-times = [list(map(int, input().split())) for _ in range(N)]
+meeting = [list(map(int, input().split())) for _ in range(N)]
 
-# 1. 끝나는 시간 기준 정렬 (끝나는 시간 같으면 시작 시간 기준)
-times.sort(key=lambda x: (x[1], x[0]))
+meeting = sorted(meeting, key=lambda x: (x[1], x[0]))
 
 count = 0
 end_time = 0
-
-# 2. greedy하게 선택
-for s, e in times:
-  if s >= end_time:
+for start, end in meeting:
+  if start >= end_time:
     count += 1
-    end_time = e
+    end_time = end
 print(count)
