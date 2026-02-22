b39d6fec22f286fe02e63bde32758d011d1a2833
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-02-22 23:58:14 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 656 ms, Memory: 134924 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,30 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
-exist = sorted(map(int, input().split()))
+cards = list(map(int, input().split()))
 M = int(input())
-should_find = list(map(int, input().split()))
+goals = list(map(int, input().split()))
 
-def lower_bound(target):
-  start, end = 0, len(exist)
-  while start < end:
-    mid = (start + end) // 2
-    if exist[mid] >= target:
-      end = mid
-    else:
-      start = mid + 1
-  return start
+freq = {}
+for card in cards:
+  freq[card] = freq.get(card, 0) + 1
 
-def upper_bound(target):
-  start, end = 0, len(exist)
-  while start < end:
-    mid = (start + end) // 2
-    if exist[mid] > target:
-      end = mid
-    else:
-      start = mid + 1
-  return start
-
-def get_cards_num(num):
-  return upper_bound(num) - lower_bound(num)
-
-for x in should_find:
-  print(get_cards_num(x), end=' ')
+res = []
+for goal in goals:
+  res.append(freq.get(goal, 0))
+print(' '.join(map(str, res)))
