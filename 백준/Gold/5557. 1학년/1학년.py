96082160ed4077eec8b3ece173a58925a4a97fda
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-03-15 22:55:34 +0900

    [Gold V] Title: 1학년, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/5557. 1학년/1학년.py
+++ 백준/Gold/5557. 1학년/1학년.py
@@ -1,19 +1,23 @@
+import sys
+sys.setrecursionlimit(10**6)
+
 N = int(input())
-numbers = list(map(int, input().split()))
+seq = list(map(int, input().split()))
+target = seq[-1]
 
-MAX = 20
-dp = [[0] * (MAX + 1) for _ in range(N)]
-dp[0][numbers[0]] = 1 # dp[i][v]: i번째 숫자까지 사용해서, 값 v를 만들 수 있는 경우의 수
+# dp[i][j] : i번째 수까지 계산해서 j를 만드는 경우의 수
+dp = [[-1] * 21 for _ in range(N)]
 
-for i in range(1, N - 1):
-  for v in range(MAX + 1):
-    if dp[i - 1][v] == 0:
-      continue
-    plus = v + numbers[i]
-    minus = v - numbers[i]
-    if 0 <= plus <= MAX:
-      dp[i][plus] += dp[i - 1][v]
-    if 0 <= minus <= MAX:
-      dp[i][minus] += dp[i - 1][v]
+def dfs(idx, cur, dp):
+  if cur < 0 or cur > 20:
+    return 0
+  if idx == N - 1:
+    return int(cur == target)
+  if dp[idx][cur] != -1:
+    return dp[idx][cur]
 
-print(dp[N - 2][numbers[-1]])
+  res = dfs(idx + 1, cur + seq[idx], dp) + dfs(idx + 1, cur - seq[idx], dp)
+  dp[idx][cur] = res
+  return res
+
+print(dfs(1, seq[0], dp))
