aeffcb3036794dcad535246c71f653f9553948b2
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-03-27 23:37:23 +0900

    [Bronze I] Title: 색종이, Time: 348 ms, Memory: 39196 KB, Score: 53 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,21 +1,18 @@
-# 10163 색종이
 import sys
 input = sys.stdin.readline
 
 N = int(input())
-board = [[0]*101 for _ in range(101)]
-papers = []
+board = [[0]*1001 for _ in range(1001)]
 
 for i in range(1, N+1):
   x, y, w, h = map(int, input().split())
-  papers.append((x, y, w, h))
   for r in range(y, y+h):
     for c in range(x, x+w):
       board[r][c] = i
 
 res = [0]*(N+1)
-for r in range(101):
-  for c in range(101):
+for r in range(1001):
+  for c in range(1001):
     if board[r][c] != 0:
       res[board[r][c]] += 1
 
