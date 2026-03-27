f44d8e099184033e5156b1dd4b5ea980186e68b5
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-03-27 23:42:48 +0900

    [Bronze I] Title: 색종이, Time: 300 ms, Memory: 39328 KB, Score: 53 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -3,18 +3,27 @@ input = sys.stdin.readline
 
 N = int(input())
 board = [[0]*1001 for _ in range(1001)]
+papers = []
 
+# 1. 색종이 덮기 (마지막에 덮인 색종이 번호 기록)
 for i in range(1, N+1):
   x, y, w, h = map(int, input().split())
+  papers.append((x, y, w, h))
   for r in range(y, y+h):
     for c in range(x, x+w):
       board[r][c] = i
 
+# 2. 각 색종이별 "보이는 칸"만 다시 확인
 res = [0]*(N+1)
-for r in range(1001):
-  for c in range(1001):
-    if board[r][c] != 0:
-      res[board[r][c]] += 1
+for i in range(1, N+1):
+  x, y, w, h = papers[i-1]
+  cnt = 0
+  for r in range(y, y+h):
+    for c in range(x, x+w):
+      if board[r][c] == i:
+        cnt += 1
+  res[i] = cnt
 
+# 3. 출력
 for i in range(1, N+1):
   print(res[i])
