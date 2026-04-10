547ce6d4309d4d31efe9d68f2a49ac24b47248cf
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-04-11 08:26:03 +0900

    [Silver II] Title: 조합 0의 개수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.py
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.py
@@ -1,14 +1,15 @@
-def count_factor(num, factor):
-  count = 0
-  while num >= factor:
-    count += num // factor
-    num //= factor
-  return count
-
 n, m = map(int, input().split())
+if n - m < m:
+  m = n - m
 
-# 2와 5의 개수 구하기
-two = count_factor(n, 2) - count_factor(m, 2) - count_factor(n - m, 2)
-five = count_factor(n, 5) - count_factor(m, 5) - count_factor(n - m, 5)
+def count_term(base, number):
+  cnt = 0
+  while base >= number:
+    cnt += base // number
+    base //= number
+  return cnt
+
+two = count_term(n, 2) - count_term(m, 2) - count_term(n - m, 2)
+five = count_term(n, 5) - count_term(m, 5) - count_term(n - m, 5)
 
 print(min(two, five))
