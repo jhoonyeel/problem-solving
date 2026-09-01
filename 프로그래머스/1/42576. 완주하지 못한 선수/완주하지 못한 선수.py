2ab2ab3ea3ac9bdc9777e7fa27db77709846eaf6
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-09-02 02:15:38 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.84 ms, Memory: 34.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -2,15 +2,23 @@
 # 1. 일반 리스트 비교는 불필요한 정렬 필요
 # 2. 그래서 딕셔너리 사용함. 그러나 중복 네이밍 정보 사라짐.
 # 3. 따라서, {name: count} 정보를 저장함.
+# 부록. Counter를 사용하면 Counter 객체 사이 빈도 연산 가능.
+# 번외. 주로 사용되는 라이브러리
+"""
+collections.deque
+heapq
+collections.defaultdict
+collections.Counter
+itertools.combinations / permutations
+bisect
+"""
+
+from collections import Counter
 
 def solution(participant, completion):
-    d = {}
-    for name in participant:
-        d[name] = d.get(name, 0) + 1
+    p = Counter(participant)
+    c = Counter(completion)
     
-    for name in completion:
-        d[name] -= 1
+    res = p - c
     
-    for k, v in d.items():
-        if v > 0:
-            return k
+    return next(iter(res)) # "원소가 하나뿐인 dict/Counter에서 그 key 하나 꺼내기"
