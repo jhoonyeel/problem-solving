9234324b81718372e6a4a6fb6fae26cbf4adb46c
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-09-02 01:51:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 52.98 ms, Memory: 30.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,10 +1,9 @@
 ## 오답
-# 일반 리스트 비교는 불필요한 정렬 필요
-# 그래서 딕셔너리 사용함. 그러나 중복 네이밍 정보 사라짐.
-# 따라서, {name, count} 정보를 저장함.
+# 1. 일반 리스트 비교는 불필요한 정렬 필요
+# 2. 그래서 딕셔너리 사용함. 그러나 중복 네이밍 정보 사라짐.
+# 3. 따라서, {name: count} 정보를 저장함.
 
 def solution(participant, completion):
-    idx = 0
     d = {}
     for name in participant:
         d[name] = d.get(name, 0) + 1
