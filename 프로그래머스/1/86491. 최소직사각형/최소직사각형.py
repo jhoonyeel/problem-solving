a14516f49f76bb7658c92bba0c3f2d0791c0d484
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-09-03 02:32:36 +0900

    [level 1] Title: 최소직사각형, Time: 0.66 ms, Memory: 12.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,3 +1,8 @@
+## 풀이
+# 기존 풀이가 언 뜻 보이고, 완전탐색 섹션의 문제라, dfs 활용하는 것인지 넘어갈 뻔 했음.
+# 다만, 문제를 봤을 때 하나의 명함의 값을 (w <= h) 구조로 유지하도록 swap하는 방식이 떠오름.
+# 충분히 swap과 max값을 순회하며 다룬다면, 계산 가능할 것으로 판단하여 풀이함.
+
 def solution(sizes):
     max_w, max_h = float('-inf'), float('-inf')
     for w, h in sizes:
