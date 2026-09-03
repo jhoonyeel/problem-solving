7f527580d87bb8215ee1c39c02ba4c29c4305e43
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-09-04 01:30:13 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,3 +1,8 @@
+## 풀이
+# 예시 방법 그대로 1,2,3 순서대로 진행하면 되겠다 생각함. 딱히 다른 개선 구조가 없어보임.
+# i, j, k 슬라이싱, sorted 활용해서 원본 배열 유지.
+# 처음부터 저장할 공간 고려하여 빈 list 선언.
+
 def solution(array, commands):
     res = []
     for i, j, k in commands:
