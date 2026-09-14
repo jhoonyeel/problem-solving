ae411073312688a29f935d0ac632fae89b91b7d5
jhoonyeel <108325028+jhoonyeel@users.noreply.github.com>
2026-09-14 20:58:27 +0900

    [level 4] Title: 징검다리, Time: 93.94 ms, Memory: 13.9 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -1,9 +1,11 @@
+## 오답
 # 바위 2개 다 제거해서, 해당 step 중 "사이 거리의 최솟값" 찾으면 되어 보임.
 # 그렇지만 어떻게? -> 시뮬레이션? 다른 알고리즘? -> 직관이 보이진 않음.
 # 따라서 전부 다 해보고 step들의 최솟값 비교하려고 함.
 # 근데 복잡도 보면, dfs 바로 터지는 수준. 추가로 심지어 제거하는 바위 숫자도 변동.
 # "제거할 바위 조합을 전부 만들어 보는 순간" -> 무조건 복잡도 터짐.
 
+## 풀이
 # 사고 전환 : 최소 거리를 x라고 미리 정해놓으면, 바위를 왼쪽부터 한 번만 순회하면서 x를 만족시키기 위해 몇 개를 제거해야 하는지 셀 수 있을까?
 # -> 가능/불가능만 판정할 수 있음.
 # 이분 탐색: 정답 후보를 가정 -> 참 / 거짓 판별 -> 단조성을 이용하여 나머지 후보 범위를 버림.
@@ -13,6 +15,7 @@
 
 def solution(distance, rocks, n):
     srt = sorted(rocks)
+    srt.append(distance)
     
     def check(candidate_minest):
         remove = 0
@@ -22,9 +25,6 @@ def solution(distance, rocks, n):
                 remove += 1
             else:
                 prev_location = r
-        
-        if distance - prev_location < candidate_minest:
-            remove += 1
         
         return remove <= n
     
